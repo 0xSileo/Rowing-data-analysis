13a58849fa8d19a23b9f9d52cef0315381c48187
sileooo <127872023+sileooo@users.noreply.github.com>
2023-12-19 16:10:10 +0100

    Improved functionality

--- ggplot_analysis.R
+++ ggplot_analysis.R
@@ -24,21 +24,24 @@ season_df[c(4,6,12)] <- rep(NULL,3)
 colnames(season_df)[c(4,5,8)] <- c("Work.Time.Seconds","Rest.Time.Seconds","SPM")
 season_df$Pace.Seconds <- (season_df$Work.Time.Seconds/season_df$Work.Distance)*500
 season_df <- Filter(function(x)!all(is.na(x)), season_df)
+season_df <- season_df[!season_df$Work.Distance %in% IGNORED_DISTANCES, ]
 head(season_df)
 
-ggplot(season_df, aes(x = SPM, y = Pace.Seconds, color = as.character(Work.Distance), alpha = as.POSIXct(Date))) + 
-  geom_point(size = 3) +
+ggplot(season_df, aes(x = SPM, y = Pace.Seconds, color = factor(Work.Distance, levels = sort(unique(Work.Distance))), alpha = as.POSIXct(Date))) + 
+  geom_point(size = 4) +
   scale_x_continuous(breaks = breaks_width(1)) + 
   # scale_color_continuous(trans = "log10") + si color est numerique
   scale_y_continuous(breaks = breaks_width(4), labels = format_seconds) + 
-  theme_linedraw(base_size = 16)
+  scale_color_manual(values = rainbow(length(unique(season_df$Work.Distance)), rev=TRUE)) +
+  labs(color = "Distance (m)") +
+  theme_solarized(base_size = 16) 
 
 
 
 # Group by column 'W.D' and summarize to get the minimum time for each 'W.D' value
 best_times_df <- subset(season_df, !(Work.Distance %in% IGNORED_DISTANCES)) %>%
   group_by(Work.Distance) %>%
-  summarize(Pace.Seconds = min(Pace.Seconds))
+  summarize(Pace.Seconds = min(Pace.Seconds)) 
 
 best_times_df$Work.Distance.Log <- 5 * log2(best_times_df$Work.Distance/2000)
 
@@ -60,7 +63,7 @@ ggplot(season_df, aes(x = Work.Distance, y = Pace.Seconds, color = SPM)) +
 # Styling
   
   scale_color_viridis_c(direction = -1) +
-  scale_x_continuous(n.breaks=10, expand=c(0,0), limits=c(0,11000)) + 
+  scale_x_continuous(n.breaks=20, expand=c(0,0), limits=c(0,max(season_df$Work.Distance)*1.02)) + 
   coord_cartesian(ylim=c(min(season_df$Pace.Seconds),max(season_df$Pace.Seconds))) +
   # scale_color_continuous(trans = "log10") + si color est numerique
   scale_y_continuous(breaks = breaks_width(4), labels = format_seconds, expand = expansion(add= c(10,2))) + 
--- group_analysis.R
+++ group_analysis.R
@@ -1,9 +1,11 @@
+library(ggplot2)
+
 df <- read.csv("Group-data/group_bests.csv")
 df$Total.Time.Seconds <- df$Time.Hours*3600 + df$Time.Minutes * 60 + df$Time.Seconds + df$Time.Tenths/10
 df$Pace <- df$Total.Time.Seconds / df$Distance * 500 
 
 ggplot(df, aes(x = Distance, y = Pace, color = Name)) + 
-  geom_point(size = 3) + 
+  geom_point() + 
   geom_smooth(method = "lm", formula = y ~ log2(x/2000), 
              alpha=0.08, linewidth = 0.4,
               fullrange=TRUE, n=1000, se=F)
